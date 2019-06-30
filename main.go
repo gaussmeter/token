7c7e4c3cf5681ebe5878856964e1c78e1f68d5b8
Leland Sindt <leland@sway.org>
2019-06-30 11:50:03 -0500

    remove todo

--- main.go
+++ main.go
@@ -1,9 +1,5 @@
 package main
 
-//ToDo: renew Token.
-// gofunction/thread that occasionally retrieves the /secret/tToken
-// checks to see if expires soon and renews.
-
 import (
   "bytes"
   "encoding/json"
